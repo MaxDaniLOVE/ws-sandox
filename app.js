32417936eaabb4e2d9df5aa0ba99458a1fa043b8
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-09-13 22:08:31 +0300

    feat: add messages for all users

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const { Server } = require('ws');
+const admin = require('firebase-admin');
+
+const serviceAccount = require("./ws-nodejs-b191b-firebase-adminsdk-42p8l-5d99b91e69.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ws-nodejs-b191b.firebaseio.com"
+});
+
 const port = process.env.PORT || 8080;
 
 const wss = new Server({ port });
 
 wss.on('connection', ws => {
   ws.on('message', message => {
-    ws.send(message)
+    wss.clients.forEach(function each(client) {
+      client.send(message);
+    })
   });
-
+  
   ws.send('ho!');
 });
