a0eb5bbc0580d8afd83d0b38861357bcedb3c5a3
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-09-13 16:58:25 +0300

    websocket starting point

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const WebSocket = require('ws');
+const { Server } = require('ws');
+const port = process.env.PORT || 8080;
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new Server({ port });
 
 wss.on('connection', ws => {
   ws.on('message', message => {
