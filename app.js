b58eea37d46d04db565b395e7aaeb2ac67b89773
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2020-09-27 19:21:12 +0300

    add path

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { Server } = require('ws');
 const admin = require('firebase-admin');
+const express = require('express');
 
 const serviceAccount = require("./config/serviceAccount");
 
@@ -9,8 +10,13 @@ admin.initializeApp({
 });
 
 const port = process.env.PORT || 8080;
+const app = express();
 
-const wss = new Server({ port });
+const server = app.listen(port, () => {
+  console.log('Listening', server.address());
+});
+
+const wss = new Server({ server, path: '/ws' });
 
 wss.on('connection', ws => {
   ws.on('message', message => {
