27491d89624cbd09b87cfe09b0179b8aea7982e3
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2021-01-05 23:09:46 +0300

    fix

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use((req, res, next) => {
 app.use(cors(corsOptions));
 
 app.use(cookieParser());
+app.set('trust proxy', 1)
 
 const server = app.listen(port, () => {
   console.log('Listening', server.address());
