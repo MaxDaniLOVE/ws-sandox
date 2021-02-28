2b97b5810524f10bd86260e7fc66e983cfe5c5eb
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2021-02-28 16:09:24 +0300

    add spinner

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const { Server } = require('ws');
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user-routes');
-const mongoose = require('mongoose');
+const initSocket = require('./ws');
 require('dotenv').config();
 const authMiddleware = require('./middleware/token-auth');
+const initDatabase = require('./utils/initDatabase');
+
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', process.env.ORIGIN);
@@ -21,24 +22,7 @@ app.use((req, res, next) => {
     next();
 });
 const server = app.listen(port, () => console.log('Listening', server.address()));
-const wss = new Server({ server, path: '/ws' });
 app.use('/user', userRoutes);
 app.use(authMiddleware);
-wss.on('connection', ws => {
-  ws.on('message', message => {
-    wss.clients.forEach((client) => {
-      client.send(message);
-    })
-  });
-  ws.send(JSON.stringify({ message: 'ho!' }));
-});
-try {
-    mongoose
-        .connect(
-            process.env.DB_URL,
-            { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }
-        );
-    console.log('Success db connection');
-} catch (e) {
-    console.log('Failed db connection');
-}
+initSocket(server);
+initDatabase();
