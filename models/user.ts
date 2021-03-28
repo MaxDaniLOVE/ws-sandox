ec55bea40a60795c3009ae9c7c4a8e0b6b9ae112
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2021-03-28 18:20:13 +0300

    add avatar uploading (#3)

--- models/user.ts
+++ models/user.ts
@@ -4,12 +4,14 @@ export interface IUser extends Document {
 	userName: string;
 	email: string;
 	password: string;
+	avatar: Buffer | null;
 }
 
 const userSchema = new mongoose.Schema({
 	userName: { type: String, required: true },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
+	avatar: { type: Buffer, required: false },
 });
 
 export const User = mongoose.model<IUser>('User', userSchema);
