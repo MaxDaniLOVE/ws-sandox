ebab8a8c94d2c7df373f1f3131df5df2f45b0bd2
Maks Daniłaŭ <47792447+MaxDaniLOVE@users.noreply.github.com>
2021-04-04 17:50:22 +0300

    add secure

--- routes/user-routes.ts
+++ routes/user-routes.ts
@@ -48,7 +48,7 @@ router.post('/sign-in', async (req, res, next) => {
 		if (cookie === undefined) {
 			let randomNumber=Math.random().toString();
 			randomNumber=randomNumber.substring(2,randomNumber.length);
-			res.cookie('cookieName',randomNumber, { maxAge: 900000, sameSite: 'none' });
+			res.cookie('cookieName',randomNumber, { maxAge: 900000, sameSite: 'none', secure: true });
 			console.log('cookie created successfully');
 		} else {
 			console.log('cookie exists', cookie);
